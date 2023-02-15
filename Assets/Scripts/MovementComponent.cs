dd13727435930304e7d9afe400ebd0727fce23c4
Aroys13 <aroyub01@ucm.es>
2023-02-15 10:06:19 +0100

    Script Movemenet
    Primera versión del movimiento realizada

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MovementComponent : MonoBehaviour
 {
+    #region Parameters
+    [SerializeField]
+    private float _myForce; //fuerza con la que se mueve el player
+    #endregion
+
+    #region References
+    private Rigidbody _myRigidBody2D; //referencia al rigidbody del player
+    #endregion
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myRigidBody2D= GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movementX = Input.GetAxisRaw("Horizontal");  //este valor puede ser -1, 0 o 1 indicando si va hacia la derecha, izquierda o no hay movimiento (funciona con joystick)
+        Vector2 playerPosition = transform.position;
+         
+        playerPosition = playerPosition + new Vector2 (movementX, 0f) * _myForce * Time.deltaTime;
+        transform.position = playerPosition;
     }
 }
