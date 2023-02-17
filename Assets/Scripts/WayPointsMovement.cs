bd5b53707fcb5441d89938a636dd23f5bf7a8ae0
aliciaalonsoo <alialo09@ucm.es>
2023-02-17 12:46:17 +0100

    Update

---
+++ Assets/Scripts/WayPointsMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointsMovement : MonoBehaviour
+{
+    //Este script sirve para cualquier gameobject que vaya a tener una ruta determinada, se puede usar tanto para los enemigos como para plataformas
+
+    [SerializeField]
+    private Transform[] waypoints; //puntos de cambio de dirección
+
+    private Vector2 siguientePosicion; //posición siguiente a la que se debe llegar
+    private int numeroSigPosicion = 0; //contador de posiciones
+
+    [SerializeField]
+    private float distanciaCambio = 1f; //distancia entre gameobject-waypoint en la que se debe cambiar de dirección
+
+    [SerializeField]
+    private float velocidad; //velocidad del gameobject
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        siguientePosicion = waypoints[0].position; //se establece la primera posición a alcanzar
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, siguientePosicion, velocidad * Time.deltaTime); /*el gameobject se mueve desde su posición hasta la siguiente
+                                                                                                                     con una velocidad determinada*/
+
+        if (Vector2.Distance(transform.position, siguientePosicion) < distanciaCambio)
+        {
+            numeroSigPosicion++; /*cuando la distancia entre el gameobject y la siguiente posición sea < 0.5f, 
+                                 la siguiente posición cambia al siguiente elemento en el array de waypoints*/
+
+            if (numeroSigPosicion == waypoints.Length) //si llega al final del array de waypoints, vuelve a la posición inicial
+            {
+                numeroSigPosicion = 0;
+            }
+            siguientePosicion = waypoints[numeroSigPosicion].position; //se establece la nueva posición
+
+        }
+    }
+}
