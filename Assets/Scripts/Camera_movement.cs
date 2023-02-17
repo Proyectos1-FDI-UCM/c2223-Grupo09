087ee442297fcd488ec0b0dcfa7b26c6ef7735d3
javalonso1 <javalo08@ucm.es>
2023-02-17 11:34:59 +0100

    Últimos cambios de la cámara básica
    Ahora hay un mínimo de la posición en Y de la cámara

--- Assets/Scripts/Camera_movement.cs
+++ Assets/Scripts/Camera_movement.cs
@@ -10,30 +10,34 @@ public class Camera_movement : MonoBehaviour
     #endregion
     #region constantes
     const float DIST_LATERAL = 2.5f;
-    const float DIST_ALT_ABAJO = 3.5f;
-    const float DIST_ALT_ARRIBA = 1.0f;
+    const float DIST_VERTICAL= 1.0f;
+    private float _posYMinim;
     #endregion
     void Start()
     {
         cam_Transform = transform;
+        _posYMinim= cam_Transform.position.y;
     }
     void Update()
     {
-        if (cam_Transform.position.x < (player_Transform.position.x - DIST_LATERAL))  //si la cámara esta mucho más a la izquierda que el jugador
+        if (cam_Transform.position.x < (player_Transform.position.x - DIST_LATERAL))    //si la cámara esta mucho más a la izquierda que el jugador
         {
             cam_Transform.Translate(new Vector2((player_Transform.position.x - DIST_LATERAL) - cam_Transform.position.x, 0)); //Se mueve a la derecha lo mismo que el jugador
         }
-        if (cam_Transform.position.x > (player_Transform.position.x + DIST_LATERAL))  //si la cámara esta mucho más a la derecha que el jugador
+        if (cam_Transform.position.x > (player_Transform.position.x + DIST_LATERAL))    //si la cámara esta mucho más a la derecha que el jugador
         {
             cam_Transform.Translate(new Vector2((player_Transform.position.x + DIST_LATERAL) - cam_Transform.position.x, 0)); //Se mueve a la izquierda lo mismo que el jugador
         }
-        if (cam_Transform.position.y > (player_Transform.position.y + DIST_ALT_ABAJO))  //si la cámara esta mucho más arriba que el jugador
+        if (cam_Transform.position.y > (player_Transform.position.y + DIST_VERTICAL))   //si la cámara esta mucho más arriba que el jugador
         {
-            cam_Transform.Translate(new Vector2(0, (player_Transform.position.y + DIST_ALT_ABAJO) - cam_Transform.position.y)); //Se mueve hacia abajo lo mismo que el jugador
+            if (cam_Transform.position.y > _posYMinim)                                    //si está más arriba de la posición mínima de Y
+            {
+                cam_Transform.Translate(new Vector2(0, (player_Transform.position.y + DIST_VERTICAL) - cam_Transform.position.y)); //Se mueve hacia abajo lo mismo que el jugador
+            }                
         }
-        if (cam_Transform.position.y < (player_Transform.position.y - DIST_ALT_ARRIBA))  //si la cámara esta mucho más debajo que el jugador
+        if (cam_Transform.position.y < (player_Transform.position.y - DIST_VERTICAL))   //si la cámara esta mucho más debajo que el jugador
         {
-            cam_Transform.Translate(new Vector2(0, (player_Transform.position.y - DIST_ALT_ARRIBA) - cam_Transform.position.y)); //Se mueve hacia arriba lo mismo que el jugador
+            cam_Transform.Translate(new Vector2(0, (player_Transform.position.y - DIST_VERTICAL) - cam_Transform.position.y)); //Se mueve hacia arriba lo mismo que el jugador
         }
     }
 }
