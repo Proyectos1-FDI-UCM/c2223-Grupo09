1b43495745bc6012002b7ee5dffd4efe600b6f03
Aroys13 <aroyub01@ucm.es>
2023-02-20 12:05:21 +0100

    Scripts comentados (Soy javier)

--- Assets/Scripts/Background_effect.cs
+++ Assets/Scripts/Background_effect.cs
@@ -5,29 +5,29 @@ using UnityEngine;
 public class Background_effect : MonoBehaviour
 {
     #region references
-    [SerializeField] private GameObject cam;
+    [SerializeField] private GameObject cam;    //referencia a la cámara (meter en el editor)
     #endregion
     #region properties
-    private float _lenght;
-    private float _startPos;
-    private float _dist;
-    private float _temp;
-    [SerializeField] private float _speed;
+    private float _lenght;                  //variable que almacena el tamaño del sprite    
+    private float _startPos;                //variable que guarda la posición de comienzo
+    private float _dist;                    //distancia que ha de moverse el script respecto al _startPos
+    private float _temp;                    //variable temporal usada para ver si se cambia el _startPos
+    [SerializeField] private float _speed;  //velociadad a la que ocurre el parallex scrollex
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-        _startPos = transform.position.x;
-        _lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+        _startPos = transform.position.x;                           
+        _lenght = GetComponent<SpriteRenderer>().bounds.size.x;     
     }
 
     // Update is called once per frame
     void Update()
     {
         _temp = cam.transform.position.x * (1 - _speed);
-        _dist = cam.transform.position.x * _speed;
-        transform.position = new Vector2(_startPos + _dist, transform.position.y);
-        if (_temp > _startPos + _lenght) _startPos += _lenght;
-        else if (_temp < _startPos - _lenght) _startPos -= _lenght;
+        _dist = cam.transform.position.x * _speed;                                      //la distancia que uno se mueve se calcula
+        transform.position = new Vector2(_startPos + _dist, transform.position.y);      //se mueve hasta la posición inicial + la distancia calculada
+        if (_temp > _startPos + _lenght) _startPos += _lenght;                          //si la cámara se mueve demasiado hacia un lado, se cambia el _startpos
+        else if (_temp < _startPos - _lenght) _startPos -= _lenght;                     //lo mismo, pero hacia el lado opuesto
     }
 }
