7fc0ad0a20065825a74c002fbfe12bb4300d72e1
aliciaalonsoo <alialo09@ucm.es>
2023-02-22 10:52:21 +0100

    Escena montada

---
+++ Assets/Scripts/DetectaPlayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectaPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private string detectionTag = "Player";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
