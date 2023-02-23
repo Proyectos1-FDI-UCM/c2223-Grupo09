9aa4ac17a152ffbece2bd6e3959461fcbd5046cf
Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
2023-02-23 16:09:43 +0100

    He mejorado el Script

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -6,29 +6,30 @@ public class KillPlayer : MonoBehaviour
 {
     #region references
     private PlayerLifeComponent _myPlayerLifeComponent; //referencia al Life Component del jugador
+    private Collider2D _playerCollider;                 //Referencia al collider del player
     private Collider2D _myBoxCollider;                  //Referencia al collider del enemigo
-    private Rigidbody2D _myRigidbody;                   //Referencia al rigidbody del enemigo
     #endregion
     void Start()
     {
         _myBoxCollider = GetComponent<Collider2D>();
-        _myRigidbody = GetComponent<Rigidbody2D>();
     }
-    private void OnCollisionEnter2D(Collision2D collision)  //Cuando un objeto colisiona con el enemigo
+    IEnumerator Sigue_ahí()     //este IEnumerator sirve para que, si el jugador no sale del trigger del enemigo, este aun así le haga daño cuando acabe su inmortalidad
+    {        
+        yield return new WaitForSeconds(2.6f);
+        if (_myBoxCollider.IsTouching(_playerCollider))
+        {
+            _myPlayerLifeComponent.Hit();
+            StartCoroutine(Sigue_ahí());
+        }            
+    }
+    private void OnTriggerEnter2D(Collider2D collision) //Cuando el enemigo colisiona con el enemigo
     {
         if (collision.gameObject.GetComponent<PlayerLifeComponent>() != null)   //se comprueba si este tiene un Script de Life Component
         {
-            _myPlayerLifeComponent = collision.gameObject.GetComponent<PlayerLifeComponent>();      //se toma el Script PlayerLifeComponent
-            _myPlayerLifeComponent.Hit();                                                           //se llama al metodo Hit de ese script
-            StartCoroutine(I_Hit());                                                                //se llama al IEnumerator I_Hit
+            _playerCollider = collision;                                                                //se toma el collider del jugador
+            _myPlayerLifeComponent = _playerCollider.gameObject.GetComponent<PlayerLifeComponent>();    //se toma el Script PlayerLifeComponent                                                            
+            _myPlayerLifeComponent.Hit();                                                               //se llama al metodo Hit de ese script
+            StartCoroutine(Sigue_ahí());
         }
-    }
-    IEnumerator I_Hit()     //este IEnumerator vuelve al enemigo atravesable por dos segundos (para evitar que el jugador choque con él mientras es invulnerable, lo que puede ser incómodo)
-    {
-        _myRigidbody.gravityScale = 0;
-        _myBoxCollider.enabled = false;
-        yield return new WaitForSeconds(2f);
-        _myBoxCollider.enabled = true;
-        _myRigidbody.gravityScale = 1;
     }
 }
