514606c892ba8542d294856c749e9aff9a695b70
ADDI01 <115276994+ADDI01@users.noreply.github.com>
2023-02-24 12:22:16 +0100

    borrado metodo Fixed duplicado y borrado metodo update (sin uso)

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -37,12 +37,6 @@ public class MovementComponent : MonoBehaviour
         _onGround = true;
     }
 
-    // Update is called once per frame
-    public void Update()
-    {
-        //  Walk();      //se llama al metodo en cada frame porque si no el personaje resbala
-    }
-
     private void FixedUpdate()
     {
         _onGround = Physics2D.OverlapBox(_feet.position, _feetDimension, 0f, _ground);
@@ -50,15 +44,12 @@ public class MovementComponent : MonoBehaviour
         {
             _myRigidBody2D.AddForce(Vector2.down * _downforce);
         }
-    }
-
-    private void FixedUpdate()
-    {
-        _myRigidBody2D.velocity = new Vector2(movementX, _myRigidBody2D.velocity.y);  //la nueva posicion del personaje dada por la x que se calcule con el GetAxis * la fuerza que le queramos aplicar y sin modificar la posición y
+        //La nueva posicion del personaje dada por la x que se calcule con el GetAxis * la fuerza
+        //que le queramos aplicar y sin modificar la posición Y
+        _myRigidBody2D.velocity = new Vector2(movementX, _myRigidBody2D.velocity.y);  
+        
         movementX = 0f;
     }
-
-
     public void Walk(float direction)
     {
        // direction = Input.GetAxisRaw("Horizontal"); //este valor puede ser -1, 0 o 1 indicando si va hacia la derecha, izquierda o no hay movimiento (funciona con joystick)
