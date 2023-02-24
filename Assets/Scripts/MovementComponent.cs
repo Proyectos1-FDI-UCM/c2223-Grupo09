4e1bc3a6270f32eb41e4b55730f923dfd7a58dd8
ADDI01 <115276994+ADDI01@users.noreply.github.com>
2023-02-24 12:09:19 +0100

    salto completo

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -7,14 +7,20 @@ public class MovementComponent : MonoBehaviour
     #region Parameters
     [Header("Jump")]
     [SerializeField]
-    private float _jumpForce;
+    private float _jumpForce; //Fuerza de salto
     [HideInInspector]
-    public bool _onGround;
+    public bool _onGround; //Comprueba si esta en el suelo
     [SerializeField]
     private float _downforce; //Se activa al dejar de presionar
     [SerializeField]
+    private LayerMask _ground; //Determina que superficies son suelo
+    [SerializeField]
+    private Transform _feet;
+    [SerializeField]
+    private Vector3 _feetDimension;
+    [Header("Movement")]
+    [SerializeField]
     private float _myForce; //fuerza con la que se mueve el player
-    
     float movementX = 0f; //para asignar la dirección donde vaya a ir el jugador
     #endregion
 
@@ -37,9 +43,10 @@ public class MovementComponent : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!_onGround) //Bajada
+        _onGround = Physics2D.OverlapBox(_feet.position, _feetDimension, 0f, _ground);
+        if (!_onGround)
         {
-            _myRigidBody2D.AddForce(Vector2.down * _downforce, ForceMode2D.Impulse);
+            _myRigidBody2D.AddForce(Vector2.down * _downforce);
         }
     }
 
@@ -55,5 +62,12 @@ public class MovementComponent : MonoBehaviour
             _myRigidBody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
             _onGround = false;
         }
+    }
+
+    //Funcion que muestra los "pies" del personaje
+    private void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_feet.position, _feetDimension);
     }
 }
