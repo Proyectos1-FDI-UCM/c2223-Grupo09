a2a7c1d9908ac963b45b340f79af9b29d708cb7d
aliciaalonsoo <alialo09@ucm.es>
2023-03-11 16:14:01 +0100

    enemigo dispara terminado

--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -12,38 +12,12 @@ public class EnemyShoot : MonoBehaviour
     private float _restingTime = 2.0f;
     #endregion
 
-    #region References
-    [SerializeField]
-    private Transform _myPlayerTransform;
-
-    BulletEnemy bulletEnemy;
-    #endregion
-
-    void Start()
-    {
-        bulletEnemy = GetComponent<BulletEnemy>();
-        bulletEnemy = _bullet.GetComponent<BulletEnemy>();
-    }
-
     IEnumerator BulletTime()
     {
         for(int i = 0; i < 3; i++)
         {
            yield return new WaitForSeconds(0.2f);
            Instantiate(_bullet, _myEnemyController.position, _myEnemyController.rotation);  
-          
-            if(_myPlayerTransform.position.x > transform.position.x)
-            {
-                //Debug.Log("bala derecha");
-                 bulletEnemy.BalaRight();
-            }
-
-            else if (_myPlayerTransform.position.x < transform.position.x)
-            {
-               //Debug.Log("bala izquierda");
-                bulletEnemy.BalaLeft();
-            }
-
         }
     }
 
