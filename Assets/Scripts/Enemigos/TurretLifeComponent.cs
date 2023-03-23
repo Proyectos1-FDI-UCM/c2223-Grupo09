4dfc72ab8324d950c4d0a0c50b57e4eaa687f446
Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
2023-03-23 17:47:06 +0100

    Añadidos algunos detalles

--- Assets/Scripts/Enemigos/TurretLifeComponent.cs
+++ Assets/Scripts/Enemigos/TurretLifeComponent.cs
@@ -8,6 +8,7 @@ public class TurretLifeComponent : MonoBehaviour
     private WayPointsMovement _myWayPoints;
     private CapsuleCollider2D _myCapsuleCollider;
     private BossComponent _bossComponent;
+    private Transform _myTransform;
     private int pos;
     #endregion
     #region Parameters
@@ -55,12 +56,22 @@ public class TurretLifeComponent : MonoBehaviour
     #endregion
     private void Start()
     {
+        _myTransform = gameObject.transform;
         _myCapsuleCollider = GetComponent<CapsuleCollider2D>();
         if(_bossTurret)_bossComponent = GameObject.Find("Final Boss").GetComponent<BossComponent>();
         if (gameObject.transform.position.x > 16f && gameObject.transform.position.x < 17f) pos = 0;
         else if (gameObject.transform.position.x > 17f) pos = 1;
         else if (gameObject.transform.position.x > -6f && gameObject.transform.position.x < -4f) pos = 2;
         else if (gameObject.transform.position.x > -2f && gameObject.transform.position.x < 1f) pos = 3;
+        StartCoroutine(Entrada());
+    }
+    IEnumerator Entrada()
+    {
+        for (int i = 0; i < 80; i++)
+        {
+            _myTransform.Translate(new Vector2(0, -0.05f));
+            yield return new WaitForSeconds(0.025f);
+        }
     }
 }
 
