85cc32c7c9843b139b59e34eab8ee24bd9a9fe27
Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
2023-03-25 13:14:44 +0100

    Pulido la escena del tutorial

--- Assets/Scripts/Objetos/ButtonComponent.cs
+++ Assets/Scripts/Objetos/ButtonComponent.cs
@@ -10,6 +10,7 @@ public class ButtonComponent : MonoBehaviour
     public GameObject platform;
     private WayPointsMovement _myWayPoints;
     public Sprite flatButton;
+    private BoxCollider2D _myBoxCollider;
     
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
@@ -27,11 +28,13 @@ public class ButtonComponent : MonoBehaviour
     {
         platform.GetComponent<WayPointsMovement>().enabled = false;
         GetComponent<Animator>().enabled = false;
+        _myBoxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Flatten()
     {
         GetComponent<SpriteRenderer>().sprite = flatButton;
         GetComponent<Animator>().enabled = false;
+        _myBoxCollider.size = new Vector2(_myBoxCollider.size.x, 0.12f);
     }
 }
