911617afe98c216efaf7ae92901f5cc5586f4a45
Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
2023-04-22 14:24:00 +0200

    Security Hitboxes

---
+++ Assets/Scripts/Objetos/SecurityHitbox.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityHitbox : MonoBehaviour
+{
+    private BoxCollider2D _myBoxCollider;
+    [SerializeField] private GameObject RoomManager;
+    private ControladorDeSalas _controladorDeSalas;
+    [SerializeField] private int sala;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myBoxCollider = GetComponent<BoxCollider2D>();
+        _controladorDeSalas = RoomManager.GetComponent<ControladorDeSalas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_controladorDeSalas.Sala == sala)
+        {
+            _myBoxCollider.enabled = false;
+        }
+        else
+        {
+            _myBoxCollider.enabled = true;
+        }
+        Debug.Log(_controladorDeSalas.Sala);
+    }
+}
